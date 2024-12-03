791c66f9b192aca8ad3fd1cb459a34562a6f58d0
Thiago Pereira <167644192+thiagopereirafde755@users.noreply.github.com>
2024-12-02 21:45:22 -0300

    Add files via upload

---
+++ adm/logica-php/deletar-prof-materia.php
@@ -0,0 +1,19 @@
+<?php
+require ('../../php/conexao.php'); 
+
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM professores_materias WHERE id = $id";
+
+    if (mysqli_query($conexao, $sql)) {
+        header("Location: ../prof-materia.php"); 
+    } else {
+        echo "Erro: " . mysqli_error($conexao); 
+    }
+}
+
+// Fecha a conexão
+mysqli_close($conexao);
+?>
---
+++ aluno/pagina_aluno.php
@@ -0,0 +1,82 @@
+<?php 
+session_start(); 
+require 'logica.php'; 
+require 'nome-sala.php';
+require 'nome-aluno.php';
+
+
+$materias_salas = [];
+while ($row = mysqli_fetch_assoc($resultado_materias_salas)) {
+    $materias_salas[] = $row; 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página do Aluno</title>
+    <link rel="stylesheet" href="../css/barra-rolagem.css">
+    <link rel="stylesheet" href="../css/stylealuno1.css">
+</head>
+<body>
+<header class="header">
+    <div class="header-content">
+        <h1 style="margin-top:9px;">Bem-vindo, Aluno!</h1>
+        <div class="profile">
+            <img src="../img/user.png" alt="Ícone de usuário" class="user-img" id="profileCircle">
+            <div class="tooltip" id="tooltip">
+                <span>Nome: <?php echo $nome_aluno; ?></span><br> 
+                <span>Email: <?php echo $email_aluno;  ?></span><br>
+                <?php
+                if (count($materias_salas) > 0) {
+                    $sala_atual = $materias_salas[0]['sala_nome'];
+                    echo "<span style='margin-left: 10px;'>Sala: " . htmlspecialchars($sala_atual) . "</span>";
+                }
+                ?>
+
+            </div>
+        </div>
+    </div>
+</header>
+<aside class="sidebar" aria-label="Navegação do Aluno">
+    <?php
+    if (count($materias_salas) > 0) {
+        $sala_atual = ''; 
+        foreach ($materias_salas as $materia_sala) {
+            if ($sala_atual !== $materia_sala['sala_nome']) {
+                if ($sala_atual !== '') {
+                    echo '</div>'; 
+                }
+                $sala_atual = $materia_sala['sala_nome'];
+                echo '<div class="sala">';
+                echo '<h2 style="margin-left: 10px;">' . htmlspecialchars($sala_atual) . '</h2>';  
+                echo '<br>';
+                echo '<h2 style="margin-left: 10px;">Minhas Matérias</h2>';
+                echo '<button class="dropdown-btn">Materias</button>';
+                echo '<div class="dropdown-container">';
+            }
+         
+            echo '<a href="javascript:void(0);" class="materia" data-materia="' . $materia_sala['materia_id'] . '">' . htmlspecialchars($materia_sala['materia_nome']) . '</a>';
+        }
+        echo '</div>'; // Fecha a última div de sala
+    } else {
+        echo "<p style='color: #ffffff;'>Não há matérias associadas a esta sala.</p>";
+    }
+    ?>
+   
+    <a href="../index.html" class="sair">
+        <img src="../img/sair-icon.png" alt="Ícone de Sair">
+        Sair
+    </a>
+</aside>
+
+<div class="content">
+    <div id="atividadeContainer"> </div> <!-- atividades carregadas -->
+</div>
+
+<script src="../js/aluno1.js"></script>
+
+</body>
+</html>
