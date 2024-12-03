8d15a5685e55007318a8140854e943dc9b1d9655
Thiago Pereira <167644192+thiagopereirafde755@users.noreply.github.com>
2024-12-02 22:08:33 -0300

    Update cadastroaluno.php

--- adm/logica-php/cadastroaluno.php
+++ adm/logica-php/cadastroaluno.php
@@ -11,15 +11,17 @@ $sql_check_email = "SELECT * FROM aluno WHERE email = ?";
 $stmt = mysqli_prepare($conexao, $sql_check_email);
 mysqli_stmt_bind_param($stmt, 's', $email); 
 mysqli_stmt_execute($stmt);
-$resultado_check_email = mysqli_stmt_get_result($stmt);
+mysqli_stmt_store_result($stmt); // Armazena o resultado para verificar a quantidade de registros
 
-if (mysqli_num_rows($resultado_check_email) > 0) {
+if (mysqli_stmt_num_rows($stmt) > 0) {
     echo "<script type='text/javascript'>
     alert('Este e-mail já está registrado. Tente outro.');
     window.location.href = '../aluno.php'; 
-  </script>";
-    // die("Este e-mail já está registrado. Tente outro.");
+    </script>";
+    exit(); // Certifique-se de interromper o script aqui
 }
+
+// Inserir o novo aluno no banco de dados
 $sql_aluno = "INSERT INTO aluno (nome, email, senha) VALUES (?, ?, ?)";
 $stmt = mysqli_prepare($conexao, $sql_aluno);
 mysqli_stmt_bind_param($stmt, 'sss', $nome, $email, $senha); 
@@ -27,13 +29,12 @@ mysqli_stmt_bind_param($stmt, 'sss', $nome, $email, $senha);
 if (mysqli_stmt_execute($stmt)) {
     $aluno_id = mysqli_insert_id($conexao); 
 
-   
+    // Associar o aluno à turma
     $sql_salas = "INSERT INTO alunos_salas (aluno_id, salas_id) VALUES (?, ?)";
     $stmt = mysqli_prepare($conexao, $sql_salas);
     mysqli_stmt_bind_param($stmt, 'ii', $aluno_id, $turma); 
     
     if (mysqli_stmt_execute($stmt)) {
-       
         echo "<script type='text/javascript'>
                 alert('Cadastro realizado com sucesso!');
                 window.location.href = '../aluno.php'; // Redireciona para a página de login
@@ -44,5 +45,6 @@ if (mysqli_stmt_execute($stmt)) {
 } else {
     echo "Erro ao cadastrar aluno: " . mysqli_error($conexao);
 }
+
 mysqli_close($conexao);
 ?>
