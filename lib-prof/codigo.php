c1cb8edf79029163a0be5f578d184effd82e037c
Thiago Pereira <167644192+thiagopereirafde755@users.noreply.github.com>
2024-12-07 16:18:21 -0300

    Update codigo.php

--- lib-prof/codigo.php
+++ lib-prof/codigo.php
@@ -12,10 +12,10 @@ if (!isset($_SESSION['email'])) {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $codigo = $_POST['codigo'];
-    
     $email_professor = $_SESSION['email'];
 
-    $query = "SELECT codigo_recuperacao, data_geracao_codigo FROM professores WHERE codigo_recuperacao='$codigo' AND email='$email_professor'";
+    // Verifica o código de recuperação para o professor com o email específico
+    $query = "SELECT codigo_recuperacao, data_geracao_codigo FROM professores WHERE email='$email_professor' AND codigo_recuperacao='$codigo'";
     $result = mysqli_query($conexao, $query);
 
     if (mysqli_num_rows($result) > 0) {
@@ -23,33 +23,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $codigo_recuperacao = $row['codigo_recuperacao'];
         $data_geracao_codigo = $row['data_geracao_codigo'];
 
-        if ($codigo == $codigo_recuperacao) {
-            // Calcula a diferença entre a hora atual e a hora em que o código foi gerado
-            $data_atual = date('Y-m-d H:i:s');
-            $tempo_expiracao = strtotime($data_atual) - strtotime($data_geracao_codigo);
+        // Verifica se o código é válido e se ainda não expirou (15 minutos)
+        $data_atual = date('Y-m-d H:i:s');
+        $tempo_expiracao = strtotime($data_atual) - strtotime($data_geracao_codigo);
 
-            if ($tempo_expiracao <= 900) { 
-                echo "<script type='text/javascript'>
-                        alert('Código válido com sucesso!');
-                        window.location.href = 'alterar-senha.html'; 
-                    </script>";
-            } else {
-                echo "<script type='text/javascript'>
-                        alert('O código expirou. Solicite um novo código.');
-                        window.location.href = 'senha-recupera.php'; 
-                    </script>";
-            }
+        if ($tempo_expiracao <= 900) { // 900 segundos = 15 minutos
+            echo "<script type='text/javascript'>
+                    alert('Código válido com sucesso!');
+                    window.location.href = 'alterar-senha.html'; 
+                  </script>";
         } else {
             echo "<script type='text/javascript'>
-                    alert('Código inválido!');
-                    window.location.href = 'verificar_codigo.php'; 
-                </script>";
+                    alert('O código expirou. Solicite um novo código.');
+                    window.location.href = 'senha-recupera.php'; 
+                  </script>";
         }
     } else {
         echo "<script type='text/javascript'>
                 alert('Código inválido!');
                 window.location.href = 'verificar_codigo.php'; 
-            </script>";
+              </script>";
     }
 }
 ?>
