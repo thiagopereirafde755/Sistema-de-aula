d272de730c0e52a45da7ce001f584f266d159462
Thiago Pereira <167644192+thiagopereirafde755@users.noreply.github.com>
2024-12-07 15:40:29 -0300

    Update login-aluno.php

--- php/login-aluno.php
+++ php/login-aluno.php
@@ -5,18 +5,26 @@ require 'conexao.php';
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-$sql = "SELECT * FROM aluno WHERE email = '$email' AND senha = '$senha' AND confirmado = 1";
+$sql = "SELECT * FROM aluno WHERE email = '$email' AND confirmado = 1";
 $result = mysqli_query($conexao, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
-    $_SESSION['aluno_id'] = $row['id'];
-    header("Location: ../aluno/pagina_aluno.php");
-    exit;
+
+    if (password_verify($senha, $row['senha'])) {
+        $_SESSION['aluno_id'] = $row['id'];
+        header("Location: ../aluno/pagina_aluno.php");
+        exit;
+    } else {
+        echo "<script type='text/javascript'>
+                alert('Email ou senha incorretos.');
+                window.location.href = '../login-aluno.php'; 
+              </script>";
+    }
 } else {
-    $sql_check = "SELECT * FROM aluno WHERE email = '$email' AND senha = '$senha'";
+    $sql_check = "SELECT * FROM aluno WHERE email = '$email'";
     $result_check = mysqli_query($conexao, $sql_check);
-    
+
     if (mysqli_num_rows($result_check) > 0) {
         echo "<script type='text/javascript'>
                 alert('Conta ainda não confirmada. Verifique seu email para confirmar sua conta.');
