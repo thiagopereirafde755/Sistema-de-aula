24dc7980cae1d7683816abd5f06ff16a1184fada
Thiago Pereira <167644192+thiagopereirafde755@users.noreply.github.com>
2024-12-07 15:37:18 -0300

    Update excluir-conta.php

--- aluno/excluir-conta.php
+++ aluno/excluir-conta.php
@@ -5,33 +5,42 @@ require '../php/conexao.php';
 if (isset($_POST['excluir_conta'])) {
     $email = mysqli_real_escape_string($conexao, $_POST['email']);
     $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
-    $query = "SELECT * FROM aluno WHERE email = '$email' AND senha = '$senha'";
+    
+    $query = "SELECT * FROM aluno WHERE email = '$email'";
     $resultado = mysqli_query($conexao, $query);
+    
     if (mysqli_num_rows($resultado) > 0) {
         $aluno = mysqli_fetch_assoc($resultado);
         $aluno_id = $aluno['id'];
-        $query_delete_salas = "DELETE FROM alunos_salas WHERE aluno_id = '$aluno_id'";
-        mysqli_query($conexao, $query_delete_salas);
-        $query_delete_aluno = "DELETE FROM aluno WHERE id = '$aluno_id'";
-        if (mysqli_query($conexao, $query_delete_aluno)) {
-            echo "<script type='text/javascript'>
-            alert('Conta excluída com sucesso!');
-            window.location.href = '../index.html'; 
-          </script>";
-            // echo "Conta excluída com sucesso!";
+
+        // Verifica se a senha fornecida corresponde ao hash armazenado
+        if (password_verify($senha, $aluno['senha'])) {
+            $query_delete_salas = "DELETE FROM alunos_salas WHERE aluno_id = '$aluno_id'";
+            mysqli_query($conexao, $query_delete_salas);
+            
+            $query_delete_aluno = "DELETE FROM aluno WHERE id = '$aluno_id'";
+            if (mysqli_query($conexao, $query_delete_aluno)) {
+                echo "<script type='text/javascript'>
+                alert('Conta excluída com sucesso!');
+                window.location.href = '../index.html'; 
+              </script>";
+            } else {
+                echo "<script type='text/javascript'>
+                alert('Erro ao excluir a conta. Tente novamente.');
+                window.location.href = 'pagina-informacoes.php'; 
+              </script>";
+            }
         } else {
             echo "<script type='text/javascript'>
-            alert('Erro ao excluir a conta. Tente novamente.');
+            alert('Senha incorreta.');
             window.location.href = 'pagina-informacoes.php'; 
-          </script>";
-            // echo "Erro ao excluir a conta. Tente novamente.";
+            </script>";
         }
     } else {
         echo "<script type='text/javascript'>
-        alert('Email ou senha incorretos.');
+        alert('Email não encontrado.');
         window.location.href = 'pagina-informacoes.php'; 
-      </script>";
-        // echo "Email ou senha incorretos.";
+        </script>";
     }
 }
 ?>
