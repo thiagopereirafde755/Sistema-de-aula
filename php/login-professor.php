8cd541240c4b4e240cfef7d06f98b4e57c045167
Thiago Pereira <167644192+thiagopereirafde755@users.noreply.github.com>
2024-12-07 15:40:54 -0300

    Update login-professor.php

--- php/login-professor.php
+++ php/login-professor.php
@@ -1,22 +1,32 @@
 <?php
 session_start();
 require 'conexao.php';
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-    $sql = "SELECT * FROM professores WHERE email = '$email' AND senha = '$senha'";
-    $result = mysqli_query($conexao, $sql); 
 
-    if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+// Consulta ao banco para obter os dados do professor com base no email
+$sql = "SELECT * FROM professores WHERE email = '$email'";
+$result = mysqli_query($conexao, $sql); 
+
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+
+    // Verificando se a senha fornecida corresponde ao hash armazenado
+    if (password_verify($senha, $row['senha'])) {
         $_SESSION['professor_id'] = $row['id'];
         header("Location: ../professor/pagina_professor.php");
         exit;
     } else {
         echo "<script type='text/javascript'>
-        alert('Email ou senha errado!');
-        window.location.href = '../login-professor.html'; // Redireciona para a página de login
-      </script>";
-        // echo "Email ou senha inválidos.";
+                alert('Email ou senha incorretos!');
+                window.location.href = '../login-professor.html'; // Redireciona para a página de login
+              </script>";
     }
+} else {
+    echo "<script type='text/javascript'>
+            alert('Email ou senha incorretos!');
+            window.location.href = '../login-professor.html'; // Redireciona para a página de login
+          </script>";
+}
 ?>
-
