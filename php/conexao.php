fdfcd5c96404c65684d3b1502d2e1cacacd45b67
Thiago Pereira <167644192+thiagopereirafde755@users.noreply.github.com>
2024-12-21 23:09:36 -0300

    Atualizar o conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,5 +1,5 @@
 <?php
-   $conexao = mysqli_connect("localhost", "root", "", "sisteam_de_aula", 3316); // O loguin, senha, nome da base e a porta
+   $conexao = mysqli_connect("localhost", "root", "", "sisteam_de_aula", 3316); // O login, senha, nome da base e a porta
   // $conexao = mysqli_connect("localhost", "root", "");
 if ($conexao) {
    $db = mysqli_select_db($conexao, "sisteam_de_aula"); 
